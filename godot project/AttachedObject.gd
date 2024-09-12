2a36d997b7af9d85e372cc285284fef166f25d7b
pierogigon <115884228+pierogigon@users.noreply.github.com>
2024-09-11 20:15:35 -0400

    adjustments

--- godot project/AttachedObject.gd
+++ godot project/AttachedObject.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-const SHOT_POWER: float = 2.0
-const FORCE_POWER: float = 10.0
+const SHOT_POWER: float = 5.0
+const FORCE_POWER: float = 20.0
 const DAMP_POWER: float = 0.16
 
 const FREE_MOVING_DISTANCE: float = 300.0
@@ -22,10 +22,26 @@ func get_rebound_force():
 		force_direction = get_point_vector().normalized()
 		return force_magnitude * force_direction
 	
-	
+
+func get_damp_value():
+	if get_point_vector().length() < FREE_MOVING_DISTANCE:
+		return 0.0
+	else:
+		var damp_value: float
+		damp_value = DAMP_POWER * (get_point_vector().length() - FREE_MOVING_DISTANCE)
+		return damp_value
+
+func get_velocity_relative_to_point():
+	return linear_velocity.dot(get_point_vector())
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Left Click"):
 		linear_velocity = SHOT_POWER * get_shot_vector()
 
 	constant_force = get_rebound_force()
+
+	if get_velocity_relative_to_point() > 0:
+		linear_damp = 0.0
+	else:
+		linear_damp = get_damp_value()
